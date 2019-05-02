39b97174c3206b9be47c0e2dc8bc266343a22470
Pierre <pierre.balland@edu.ece.fr>
2019-05-02 21:02:10 +0200

    auth utilisateur formulaire de connection

---
+++ Authentification_utilisateur.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Page d'authentification utilisateur</title>
+</head>
+<body>
+
+<h2>Sign In</h2>
+<FORM NAME ="form5" METHOD ="POST" ACTION = "traitement_authentification.php">    
+ <label>Mail :</label>    
+ <input type="text" name="Mail"> <br> <br>
+ <label>Mot de passe</label>   
+ <input name="Mot_de_passe" type="password" id="Code_securite" size="10" /> <br> <br>
+ <INPUT TYPE = "Submit" Name = "Submit2" VALUE = "Se connecter"> <br> 
+</FORM> 
+ <a href="inscription.php"> Vous n'avez pas de compte ? </a> 
+</body>
+</html>

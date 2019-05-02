cedf677c3910714d6166a0d2af2a0dbc20c5914e
Pierre <pierre.balland@edu.ece.fr>
2019-05-02 13:53:27 +0200

    fichier type de connection bdd php pour afficher des records

---
+++ connection.php
@@ -0,0 +1,28 @@
+<html>
+
+Bonjour
+
+</html>
+<?php
+require '../configure.php';
+$db_handle = mysqli_connect(DB_SERVER, DB_USER, DB_PASS );
+
+$database = "ece_amazon_128";
+$db_found = mysqli_select_db( $db_handle, $database );
+if ($db_found) {
+	$SQL="SELECT * FROM utilisateur";
+	$result = mysqli_query($db_handle, $SQL);
+      while ($db_field = mysqli_fetch_assoc($result) ) {
+             echo $db_field['id_utilisateur'] . "<br>";
+             echo $db_field['mdp'] . "<br>";
+             echo $db_field['nom'] . "<br>";
+             echo $db_field['prenom'] . "<br>";
+             echo $db_field['mail'] . "<br>";
+
+     echo "Database found";
+}
+}
+else {
+     echo "Database not found";
+}
+?>

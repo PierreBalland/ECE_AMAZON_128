938c812f68613b0cfb14fc93b39e91cb2ceaf1c7
Pierre <pierre.balland@edu.ece.fr>
2019-05-02 21:04:37 +0200

    auth admin connection admin formulaire

---
+++ Authentificationadmin.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html> 
+<html>
+
+<head>  
+
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+<title>Admin</title>   
+</head> 
+<body>
+ <h1>Authentification</h1>   
+<FORM NAME ="form9" METHOD ="POST" ACTION = "page_traitement_authentification_admin_.php">    
+ <label>Mail</label>    
+ <input type="text" name="nom"> <br> <br>    
+<label>Mot de passe:</label>    
+ <input type="text" name="Mdp"> <br> <br> 
+ <INPUT TYPE = "Submit" Name = "Submitform" VALUE = "SeConnecter">
+</FORM> 
+</body>
+</html>

9915fae80f6379efb8a3f3840fc142d622fefa65
yaraallam <yara.allam@edu.ece.fr>
2019-05-04 16:12:30 +0300

    derniere version connexion vendeur

--- connection_vendeur.php
+++ connection_vendeur.php
@@ -7,6 +7,9 @@
 <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css">
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
 <link rel="stylesheet" type="text/css" href="style.css">
+<link rel="stylesheet" href="form.css">
+<link rel="stylesheet" href="page.css">
+<link rel="stylesheet" href="vignettes.css">
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.0/jquery.min.js"></script>
 <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"></script>
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"></script>
